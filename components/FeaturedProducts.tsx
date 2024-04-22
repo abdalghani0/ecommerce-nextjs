01b9f9dff513ecd6d0a876f1fba321675e7c9c59
abdalghani0 <117361839+abdalghani0@users.noreply.github.com>
2024-04-22 18:55:15 +0300

    fixed featured products section

--- components/FeaturedProducts.tsx
+++ components/FeaturedProducts.tsx
@@ -4,7 +4,7 @@ import { supabaseServer } from "../lib/supabase/server";
 
 export default async function FeaturedProducts() {
   const supabase = await supabaseServer();
-  const { data } = await supabase.from("products").select("*").gt("rate", 4);
+  const { data } = await supabase.from("products").select("*").gt("rate", 4).limit(6);
 
   return (
     <div className="grid w-full grid-cols-2 gap-4 lg:grid-cols-3 md:gap-6">
